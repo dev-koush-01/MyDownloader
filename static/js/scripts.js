2c5e06a53c68a91e70830381cd11cc8557ed4b7b
dev-koush-01 <157644206+dev-koush-01@users.noreply.github.com>
2024-12-21 21:15:46 +0530

    Delete static directory

--- static/js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function () {
-        form.querySelector('button').innerText = 'Downloading...';
-    });
-});
